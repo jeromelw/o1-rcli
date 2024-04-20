d632c07a3dc88ff775e6da6af921292b0843c5fd
Jerome Law <jerome@ishuk.lan>
2024-04-20 12:20:33 +0800

    rcli first tool convert csv to json format.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+//rcli csv -i test.csv -d '|' -o output.json
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+    Ok(())
 }
